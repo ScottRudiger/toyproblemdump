d27e9d7f435965be2278ca0bc8d5da216c5f24f7
ScottRudiger <scottrudiger@gmail.com>
2017-09-07 23:59:13 -0700

    solves Dubstep TP

---
+++ WUBWUB.js
@@ -0,0 +1,42 @@
+// 'Dubstep' from https://www.codewars.com/kata/dubstep/train/javascript
+
+/*
+Polycarpus works as a DJ in the best Berland nightclub, and he often uses dubstep music in his performance. Recently, he has decided to take a couple of old songs and make dubstep remixes from them.
+
+Let's assume that a song consists of some number of words. To make the dubstep remix of this song, Polycarpus inserts a certain number of words "WUB" before the first word of the song (the number may be zero), after the last word (the number may be zero), and between words (at least one between any pair of neighbouring words), and then the boy glues together all the words, including "WUB", in one string and plays the song at the club.
+
+For example, a song with words "I AM X" can transform into a dubstep remix as "WUBWUBIWUBAMWUBWUBX" and cannot transform into "WUBWUBIAMWUBX".
+
+Recently, Jonny has heard Polycarpus's new dubstep track, but since he isn't into modern music, he decided to find out what was the initial song that Polycarpus remixed. Help Jonny restore the original song.
+
+Input
+
+The input consists of a single non-empty string, consisting only of uppercase English letters, the string's length doesn't exceed 200 characters
+
+Output
+
+Return the words of the initial song that Polycarpus used to make a dubsteb remix. Separate the words with a space.
+
+Examples
+
+songDecoder("WUBWEWUBAREWUBWUBTHEWUBCHAMPIONSWUBMYWUBFRIENDWUB")
+  // =>  WE ARE THE CHAMPIONS MY FRIEND
+*/
+
+const songDecoder = song => song.split('WUB').filter(word => word).join(' ');
+
+// note: could be shortened by just passing Boolean into filter; however, I prefer this implementation
+
+const assert = (actual, expected, testName) => {
+  result = actual === expected ? 'test passed' : `FAIL: ${testName}`;
+  console.log(result);
+};
+
+assert(songDecoder('AWUBBWUBC'), 'A B C', 'WUB should be replaced by 1 space');
+assert(songDecoder('AWUBWUBWUBBWUBWUBWUBC'), 'A B C', 'multiple WUBs should be replaced by only 1 space');
+assert(songDecoder('WUBAWUBBWUBCWUB'), 'A B C', 'heading or trailing spaces should be removed');
+assert(
+  songDecoder('WUBWEWUBAREWUBWUBTHEWUBCHAMPIONSWUBMYWUBFRIENDWUB'),
+  'WE ARE THE CHAMPIONS MY FRIEND',
+  'result should be a classic'
+);
