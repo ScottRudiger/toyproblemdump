8aeacf201b615ff3a2f5c3ed3dee878b6d35b261
ScottRudiger <scottrudiger@gmail.com>
2017-09-11 12:13:56 -0700

    add reference to original TP

--- whichAreIn.js
+++ whichAreIn.js
@@ -1,4 +1,4 @@
-// Which are in? from
+// Which are in? from https://www.codewars.com/kata/which-are-in/train/javascript
 
 /*Given two arrays of strings a1 and a2 return a sorted array r in lexicographical order of the strings of a1 which are substrings of strings of a2.
 
