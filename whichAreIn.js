793b6a7714fadf3cdc0cbe1cc0dc881ea93af5ff
ScottRudiger <scottrudiger@gmail.com>
2017-09-11 12:06:17 -0700

    fix tests (oops)

--- whichAreIn.js
+++ whichAreIn.js
@@ -28,26 +28,33 @@ describe('inArray', () => {
 
   context('returns elements included within strings in array2', () => {
     it('should live strong', () => {
-      array1 = ['xyz', 'live', 'strong'];
-      array2 = ['lively', 'harp', 'armstrong'];
-      expect(inArray(array1)).to.eql(['live', 'strong']);
+      const array1 = ['xyz', 'live', 'strong'];
+      const array2 = ['lively', 'harp', 'armstrong'];
+      expect(inArray(array1, array2)).to.eql(['live', 'strong']);
     });
   });
 
   context('excludes duplicates', () => {
     it('should still live strong', () => {
-      array1 = ['xyz', 'live', 'arp', 'strong'];
-      array2 = ['lively', 'alive', 'harp', 'tarp', 'strongly', 'armstrong'];
-      expect(inArray(array1)).to.eql(['live', 'strong']);
+      const array1 = ['xyz', 'live', 'strong'];
+      const array2 = ['lively', 'alive', 'harp', 'tarp', 'strongly', 'armstrong'];
+      expect(inArray(array1, array2)).to.eql(['live', 'strong']);
     });
   });
 
   context('handles case where no elements in array1 are found within elements in array2', () => {
     it('should return an empty array', () => {
-      array1 = ['tarp', 'mice', 'bull'];
+      const array1 = ['tarp', 'mice', 'bull'];
+      const array2 = ['pup', 'boy', 'etc'];
       expect(inArray(array1, array2)).to.eql([]);
-      array2 = ['pup', 'boy', 'etc'];
-      expect(inArray(array1, array2)).to.eql([]);
+    });
+  });
+
+  context('return value should be sorted alphabetically', () => {
+    it ("should be ['abc', 'def', 'xyz']", () => { // eslint-disable-line quotes
+      const array1 = ['xyz', 'def', 'abc'];
+      const array2 = ['xyzz', 'deff', 'abcc'];
+      expect(inArray(array1, array2)).to.eql(['abc', 'def', 'xyz']);
     });
   });
 
