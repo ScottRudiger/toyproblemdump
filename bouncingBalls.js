78a7df6d8b05ee382752d3023a59b66985f3885f
ScottRudiger <scottrudiger@gmail.com>
2017-09-12 17:15:33 -0700

    add tests for bouncingBalls.js

--- bouncingBalls.js
+++ bouncingBalls.js
@@ -26,4 +26,30 @@ h = 3, bounce = 0.66, window = 1.5, result is 3
 
 h = 3, bounce = 1, window = 1.5, result is -1 (Condition 2) not fullfilled).*/
 
+const bouncingBall = (h, bounce, window) => {
 
+};
+
+const expect = require('chai').expect;
+
+describe('Bouncing Balls', () => {
+
+  context('low height', () => {
+    it('should return a small number of bounces given 0.66 bounce-back rate', () => {
+      expect(bouncingBall(3, 0.66, 1.5)).to.equal(3);
+    });
+  });
+
+  context('high height', () => {
+    it('should return more bounces given a higher height', () => {
+      expect(bouncingBall(30, 0.66, 1.5)).to.equal(15);
+    });
+  });
+
+  context('high height; low gravity', () => {
+    it('should return a very large number of bounces given extremely low gravity', () => {
+      expect(bouncingBall(30, 0.9999999999, 1)).to.equal(68023942001);
+    });
+  });
+
+});
