8a7f0c3c0a7b1caef981b62b5a37fd633acc6c2a
ScottRudiger <scottrudiger@gmail.com>
2017-09-15 22:14:15 -0700

    init

---
+++ pigIt.js
@@ -0,0 +1,9 @@
+// Simple Pig Latin from
+
+/*Move the first letter of each word to the end of it, then add 'ay' to the end of the word.
+
+pigIt('Pig latin is cool'); // igPay atinlay siay oolcay*/
+
+const pigIt = (w) => {
+
+};
