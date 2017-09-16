90dd964a36a0b79fd0b1a4dbdc80be572926bf2b
ScottRudiger <scottrudiger@gmail.com>
2017-09-15 23:51:21 -0700

    add tests for pigIt.js

--- pigIt.js
+++ pigIt.js
@@ -1,9 +1,20 @@
-// Simple Pig Latin from
+// Simple Pig Latin from https://www.codewars.com/kata/simple-pig-latin/train/javascript
 
 /*Move the first letter of each word to the end of it, then add 'ay' to the end of the word.
 
 pigIt('Pig latin is cool'); // igPay atinlay siay oolcay*/
 
-const pigIt = (w) => {
+const pigIt = s => {
 
 };
+
+const expect = require('chai').expect;
+
+describe('pigIt pig latin translation function', () => {
+  it('should translate a word to pig latin', () => {
+    expect(pigIt('word')).to.equal('ordway');
+  });
+  it('should translate multiple words to pig latin', () => {
+    expect(pigIt('Pig latin is cool')).to.equal('igPay atinlay siay oolcay');
+  });
+});
