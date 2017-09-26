70a9fce27cb2643d1c8bae82f7fe0a9e3efe35e7
ScottRudiger <scottrudiger@gmail.com>
2017-09-25 19:05:22 -0700

    add tests for isValidIp.js

--- isValidIp.js
+++ isValidIp.js
@@ -20,3 +20,39 @@ Note: leading zeros (e.g. 01.02.03.04) are considered not valid in this kata!*/
 const isValidIp = address => {
 
 };
+
+const expect = require('chai').expect;
+
+describe('isValidIp function', () => {
+
+  context('a valid IP address should contain four octets--no more; no less', () => {
+    it('should return true for 1.2.3.4', () => {
+      expect(isValidIp('1.2.3.4')).to.be.true;
+    });
+    it('should return false for 1.2.3', () => {
+      expect(isValidIp('1.2.3')).to.be.false;
+    });
+    it('should return false for 1.2.3.4.5', () => {
+      expect(isValidIp('1.2.3.4.5')).to.be.false;
+    });
+  });
+
+  context('a valid IP address should have no octet with a value greater than 255', () => {
+    it('should return true for 123.45.67.89', () => {
+      expect(isValidIp('123.45.67.89')).to.be.true;
+    });
+    it('should return false for 123.456.78.90', () => {
+      expect(isValidIp('123.456.78.90')).to.be.false;
+    });
+  });
+
+  context('an IP address with leading zeros is invalid', () => {
+    it('should return false for 01.02.03.04', () => {
+      expect(isValidIp('01.02.03.04')).to.be.false;
+    });
+    it('should return false for 123.045.067.089', () => {
+      expect(isValidIp('123.045.067.089')).to.be.false;
+    });
+  });
+
+});
