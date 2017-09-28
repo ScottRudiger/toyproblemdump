da47eac55366b6e99c3ecf23294373c2bf53eadb
ScottRudiger <scottrudiger@gmail.com>
2017-09-27 19:03:31 -0700

    refactor one-liner solution

--- perimeterOfSquares.js
+++ perimeterOfSquares.js
@@ -28,14 +28,16 @@ name this outer function fib.
 
 // const perimeter = (n, p2 = 0, p1 = 4, s = 4) => !n ? s : perimeter(n - 1, p1, p2 + p1, s + p2 + p1);
 
-const perimeter = (n, s5 = Math.sqrt(5), p = (1 + s5) / 2, pn = (1 - s5) / 2) => {
-  let s = 0;
-  n += 2;
-  while (n--) {
-    s += Math.round((1 / s5) * (Math.pow(p, n) - Math.pow(pn, n))) * 4;
-  }
-  return s;
-};
+// const perimeter = (n, s5 = Math.sqrt(5), p = (1 + s5) / 2, pn = (1 - s5) / 2) => {
+//   let s = 0;
+//   n += 2;
+//   while (n--) {
+//     s += Math.round((1 / s5) * (Math.pow(p, n) - Math.pow(pn, n))) * 4;
+//   }
+//   return s;
+// };
+
+const perimeter = (n, m = n + 2, s = 0, s5 = Math.sqrt(5)) => m ? perimeter(n, --m, s + Math.round(1 / s5 * (Math.pow((1 + s5) / 2, m) - Math.pow((1 - s5) / 2, m))) * 4) : s;
 
 const expect = require('chai').expect;
 
