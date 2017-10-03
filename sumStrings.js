2d1f92f80e51bdc79a10646989822d2964e80646
ScottRudiger <scottrudiger@gmail.com>
2017-10-03 00:22:46 -0700

    add tests for sumStrings.js

--- sumStrings.js
+++ sumStrings.js
@@ -12,3 +12,14 @@ A string representation of an integer will contain no characters besides the ten
 const sumStrings = (a, b) => {
 
 };
+
+const {expect} = require('chai');
+
+describe('sumStrings function', () => {
+  it(`should return '3' given '1', '2'`, () => expect(sumStrings('1', '2')).to.equal('3'));
+  it(`should return '579' given '123', '456'`, () => expect(sumStrings('123', '456')).to.equal('579'));
+  it(`should return '9007199254740991' given '4503599627370495.5', '4503599627370495.5'`, () => {
+    const n = `${Number.MAX_SAFE_INTEGER / 2}`;
+    expect(sumStrings(n, n)).to.equal(`${Number.MAX_SAFE_INTEGER}`);
+  });
+});
