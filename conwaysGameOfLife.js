9da209b06b440e412c21bbbabf8a3a96e785b178
ScottRudiger <scottrudiger@gmail.com>
2017-10-02 22:25:04 -0700

    refactored solution--see initial solution for more readable version

--- conwaysGameOfLife.js
+++ conwaysGameOfLife.js
@@ -15,23 +15,13 @@ console.log(htmlize(cells));
 trace (htmlize cells)*/
 /*eslint-disable curly, one-var*/
 
-const countNeighbors = (c, i, j) => (t = 0, d = [-1, 0, 1], d.map(k => d.map(l => (k || l) && c[i + k] && c[i + k][j + l] ? t++ : 0)), t);
+const countNeighbors = (c, i, j) => (n = 0, d = [-1, 0, 1], d.map(k => d.map(l => (k || l) && c[i + k] && c[i + k][j + l] ? n++ : 0)), n);
 
 const padCells = c => (c = c.map(r => r.slice()), c.unshift(c[0].map(v => 0)), c.push(c[0].map(v => 0)), c.map(r => (r.unshift(0), r.push(0))), c);
 
 const cropCells = c => ((t = c => c[0].some(v => v) ? 0 : (c.shift(), t(c)))(c), (b = c => c[c.length - 1].some(v => v) ? 0 : (c.pop(), b(c)))(c), (l = c => c.some(r => r[0]) ? 0 : (c.map(r => r.shift()), l(c)))(c), (r = c => c.some(r => r[r.length - 1]) ? 0 : (c.map(r => r.pop()), r(c)))(c), c);
 
-const getGeneration = (cells, generation) => {
-  if (!generation) return cropCells(cells);
-  const copyToCount = padCells(cells), copyToMutate = padCells(cells);
-  for (let i = 0; i < copyToCount.length; i++) {
-    for (let j = 0; j < copyToCount[i].length; j++) {
-      const count = countNeighbors(copyToCount, i, j);
-      copyToMutate[i][j] = count === 2 ? copyToMutate[i][j] : count === 3 ? 1 : 0;
-    }
-  }
-  return getGeneration(copyToMutate, generation - 1);
-};
+const getGeneration = (c, g) => !g ? cropCells(c) : (t = padCells(c), m = padCells(c), t.map((r, i) => r.map((_, j) => (n = countNeighbors(t, i, j), m[i][j] = n === 2 ? m[i][j] : n === 3 ? 1 : 0))), getGeneration(m, g - 1));
 
 const {expect} = require('chai');
 
