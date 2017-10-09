2b1cf827b23bd7b082aa66e224ca033db81ab8fd
ScottRudiger <scottrudiger@gmail.com>
2017-10-09 02:14:02 -0700

    initial solution

--- price.js
+++ price.js
@@ -9,10 +9,17 @@ console.log(price); // 13,253.51
 Numbers should use the standard comma for every 3 numbers and dot to separate the cents, cents need to be truncated to 2 decimals, in the case that the decimal part of the number is 1 character long or none you should add 0's so that the result will always have 2 decimal characters, the function will also evaluate negative numbers.
 
 function should return a string 'NaN' if the input is not a valid number*/
-/*eslint-disable quotes*/
+/*eslint-disable quotes, curly*/
 
 const numberToPrice = n => {
-
+  if (isNaN(n)) return 'NaN';
+  n = [...`${n}`].reverse();
+  d = n.indexOf('.') === -1 ? '.00' : n.slice(0, n.indexOf('.') + 1).slice(-3).reverse().join('').padEnd(3, '0');
+  n = n.slice(n.indexOf('.') + 1);
+  for (let i = 3; i < n.length; i += 4) {
+    n.splice(i, 0, ',');
+  }
+  return n.reverse().join('') + d;
 };
 
 const {expect} = require('chai');
