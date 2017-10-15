29d97aac02a277eb45610258f8194386772ecc5b
ScottRudiger <scottrudiger@gmail.com>
2017-10-14 23:59:16 -0700

    init

---
+++ multiLineTask.js
@@ -0,0 +1,15 @@
+// Multi Line Task++: Hello World from https://www.codewars.com/kata/5935558a32fb828aad001213/train/javascript
+
+/*
+You need to write a function f that returns the string Hello, world!.
+
+Requirement: Every line must have at most 2 characters, and total number of lines must be less than 40.
+
+Hint: It's possible to complete this in 28 lines only.
+
+(Even) harder verion: https://www.codewars.com/kata/59a421985eb5d4bb41000031
+*/
+
+f=_=>
+
+
