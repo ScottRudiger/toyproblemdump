84d8d99c1b66f29ae40cc8486cebe907275a14c1
ScottRudiger <scottrudiger@gmail.com>
2017-10-16 22:11:23 -0700

    add tests

--- multiLineTask.js
+++ multiLineTask.js
@@ -8,8 +8,23 @@ Requirement: Every line must have at most 2 characters, and total number of line
 Hint: It's possible to complete this in 28 lines only.
 
 (Even) harder verion: https://www.codewars.com/kata/59a421985eb5d4bb41000031
-*/
+*//*eslint-disable*/
 
-f=_=>
+f=
+_=>
+''
 
+const {expect} = require('chai');
 
+describe('f function', () => {
+  const code = f.toString().split('\n');
+  it(`should return 'Hello, world!'`, () => {
+    expect(f()).to.equal('Hello, world!');
+  });
+  it('should have no line longer than 2 characters', () => {
+    expect(code.every(line => line.length < 3)).to.be.true;
+  });
+  it('should be less than 40 lines', () => {
+    expect(code.length < 40).to.be.true;
+  });
+});
