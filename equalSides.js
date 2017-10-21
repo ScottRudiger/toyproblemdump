432a158af16cfca3ebfc511a420015be5ba2dab4
ScottRudiger <scottrudiger@gmail.com>
2017-10-20 22:50:46 -0700

    fix bug in tests

--- equalSides.js
+++ equalSides.js
@@ -38,7 +38,7 @@ const {expect} = require('chai');
 
 describe('findEvenIndex function', () => {
   it('should return 3', () => expect(findEvenIndex([1, 2, 3, 4, 3, 2, 1])).to.equal(3));
-  it('should return 1', () => expect(findEvenIndex([1, 2, 3, 4, 5, 6])).to.equal(1));
+  it('should return 1', () => expect(findEvenIndex([1, 100, 50, -51, 1, 1])).to.equal(1));
   it('should return -1', () => expect(findEvenIndex([1, 2, 3, 4, 5, 6])).to.equal(-1));
   it('should return -1', () => expect(findEvenIndex([20, 10, 30, 10, 10, 15, 35])).to.equal(3));
 });
