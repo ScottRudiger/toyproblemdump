f4b95e977a20e24b577fa0220a711731390f6d32
ScottRudiger <scottrudiger@gmail.com>
2017-10-30 02:12:01 -0700

    add tests

--- alphaIndex.js
+++ alphaIndex.js
@@ -16,3 +16,12 @@ Should return "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 1
 const alphabetPosition = text => {
 
 };
+
+const {expect} = require('chai');
+
+describe('alphabetPostion function', () => {
+  it('should convert letters to their position in the alphabet', () => {
+    expect(alphabetPosition('The sunset sets at twelve o\' clock.')).to.equal('20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11');
+    expect(alphabetPosition('The narwhal bacons at midnight.')).to.equal('20 8 5 14 1 18 23 8 1 12 2 1 3 15 14 19 1 20 13 9 4 14 9 7 8 20');
+  });
+});
