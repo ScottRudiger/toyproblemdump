6fe92c1cd589b66cea2da8c3e7fb8bec0c558b57
ScottRudiger <scottrudiger@gmail.com>
2018-01-12 00:01:31 -0800

    add tests

--- subarrayToSort.js
+++ subarrayToSort.js
@@ -13,3 +13,20 @@ Examples:
 const findSubArrayToSort = arr => {
 
 };
+
+const {expect} = require('chai');
+
+describe('findSubArrayToSort function', () => {
+  it('should return indices [3, 8]', () => {
+    const arr = [10, 12, 20, 30, 25, 40, 32, 31, 35, 50, 60];
+    expect(findSubArrayToSort(arr)).to.eql([3, 8]);
+  });
+  it('should return indicies [2, 5]', () => {
+    const arr = [0, 1, 15, 25, 6, 7, 30, 40, 50];
+    expect(findSubArrayToSort(arr)).to.eql([2, 5]);
+  });
+  it('should return indices [2, 10]', () => {
+    const arr = [5, 7, 9, 13, 16, 12, 15, 8, 14, 18, 17, 20];
+    expect(findSubArrayToSort(arr)).to.eql([2, 10]);
+  });
+});
