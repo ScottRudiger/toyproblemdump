6548470040495d7ae184ef412b02606d49176d8a
ScottRudiger <scottrudiger@gmail.com>
2018-01-18 23:42:29 -0800

    init

---
+++ kClosest.js
@@ -0,0 +1,17 @@
+// from https://www.geeksforgeeks.org/find-k-closest-elements-given-value/
+/*
+Find k closest elements to a given value
+3.1
+Given a sorted array arr[] and a value X, find the k closest elements to X in arr[].
+  Examples:
+
+Input: K = 4, X = 35
+arr[] = {
+  12, 16, 22, 30, 35, 39, 42,
+  45, 48, 50, 53, 55, 56}
+Output: 30 39 42 45
+*/
+
+const findClosest = (arr, k) {
+
+};
