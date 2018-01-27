a47047d12bd3b7d1daf7ae1057bf0c0b622967ab
ScottRudiger <scottrudiger@gmail.com>
2018-01-26 23:58:40 -0800

    init

---
+++ common3.js
@@ -0,0 +1,21 @@
+// Find common elements in three sorted arrays from https://www.geeksforgeeks.org/find-common-elements-three-sorted-arrays/
+
+/*
+Given three arrays sorted in non-decreasing order, print all common elements in these arrays.
+
+Examples:
+
+ar1[] = {1, 5, 10, 20, 40, 80}
+ar2[] = {6, 7, 20, 80, 100}
+ar3[] = {3, 4, 15, 20, 30, 70, 80, 120}
+Output: 20, 80
+
+ar1[] = {1, 5, 5}
+ar2[] = {3, 4, 5, 5, 10}
+ar3[] = {5, 5, 10, 20}
+Output: 5, 5
+*/
+
+const findCommon = (a1, a2, a3) => {
+
+};
