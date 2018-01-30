a7cff7bb007ddfad36bfd1df69b79aeb9a6438ac
ScottRudiger <scottrudiger@gmail.com>
2018-01-29 23:56:43 -0800

    init

---
+++ closestPair2.js
@@ -0,0 +1,18 @@
+// Given a sorted array and a number x, find the pair in array whose sum is closest to x
+// from https://www.geeksforgeeks.org/given-sorted-array-number-x-find-pair-array-whose-sum-closest-x/
+
+/*
+Given a sorted array and a number x, find a pair in array whose sum is closest to x.
+
+Examples:
+
+Input: arr[] = {10, 22, 28, 29, 30, 40}, x = 54
+Output: 22 and 30
+
+Input: arr[] = {1, 3, 4, 7, 10}, x = 15
+Output: 4 and 10
+*/
+
+const findClosestPair = (arr, x) => {
+
+};
