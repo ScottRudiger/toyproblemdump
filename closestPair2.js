465659a6aa7bb75578eab76b89dfbcbdb77df374
ScottRudiger <scottrudiger@gmail.com>
2018-01-30 00:01:52 -0800

    add tests

--- closestPair2.js
+++ closestPair2.js
@@ -16,3 +16,16 @@ Output: 4 and 10
 const findClosestPair = (arr, x) => {
 
 };
+
+const {expect} = require('chai');
+
+describe('findClosestPair function', () => {
+  it('should return [22, 30]', () => {
+    const arr = [10, 22, 28, 29, 30, 40];
+    expect(findClosestPair(arr, 54)).to.eql([22, 30]);
+  });
+  it('should return [4, 10]', () => {
+    const arr = [1, 3, 4, 7, 10];
+    expect(findClosestPair(arr, 15)).to.eql([4, 10]);
+  });
+});
