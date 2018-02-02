4eed3fc6f3fdfd8811c09424bb3f2a789c6f019a
ScottRudiger <scottrudiger@gmail.com>
2018-02-01 23:57:49 -0800

    init

---
+++ countOnes.js
@@ -0,0 +1,20 @@
+// Count 1’s in a sorted binary array from https://www.geeksforgeeks.org/count-1s-sorted-binary-array/
+
+/*
+Given a binary array sorted in non-increasing order, count the number of 1’s in it.
+
+Examples:
+
+Input: arr[] = {1, 1, 0, 0, 0, 0, 0}
+Output: 2
+
+Input: arr[] = {1, 1, 1, 1, 1, 1, 1}
+Output: 7
+
+Input: arr[] = {0, 0, 0, 0, 0, 0, 0}
+Output: 0
+*/
+
+const countOnes = arr => {
+
+};
