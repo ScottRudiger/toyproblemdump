ee402175c6644dcbc7f207ef2efbc077d66bc690
ScottRudiger <scottrudiger@gmail.com>
2018-02-03 10:03:25 -0800

    O(log n) logarithmic solution

--- countOnes.js
+++ countOnes.js
@@ -16,7 +16,24 @@ Output: 0
 */
 
 // inefficient solution O(n)
-const countOnes = arr => arr.lastIndexOf(1) + 1;
+// const countOnes = arr => arr.lastIndexOf(1) + 1;
+
+// more efficient solution using binary search - O(log n)
+const countOnes = (arr, low = 0, high = arr.length - 1) => {
+  // calculate midpoint
+  const mid = low + (high - low) / 2 | 0; // floor in case of even length (not covered)
+  // if high >= low
+  if (high >= low) {
+    // check if element at mid is the last index of 1 or last index of arr
+    if (arr[mid] && !arr[mid + 1]) return mid + 1;
+    // check if last 1 is to the right
+    if (arr[mid] && arr[mid + 1]) return countOnes(arr, mid + 1, high);
+    // check if last 1 is to the left
+    if (!arr[mid]) return countOnes(arr, 0, mid - 1);
+  }
+  // otherwise, high < low, i.e. high has decremented to -1 (no 1's found); return 0
+  return 0;
+};
 
 const {expect} = require('chai');
 
@@ -32,5 +49,9 @@ describe('countOnes function', () => {
   it('should return 0', () => {
     const arr = [0, 0, 0, 0, 0, 0, 0];
     expect(countOnes(arr)).to.equal(0);
+  });
+  it('should return 1', () => {
+    const arr = [1, 0, 0, 0, 0, 0, 0];
+    expect(countOnes(arr)).to.equal(1);
   });
 });
