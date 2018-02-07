9c071a56a5ffdc938221a38b35a606806214c367
ScottRudiger <scottrudiger@gmail.com>
2018-02-06 23:39:45 -0800

    initial solution

--- whoLikes.js
+++ whoLikes.js
@@ -12,8 +12,13 @@ likes ["Alex", "Jacob", "Mark", "Max"] // must be "Alex, Jacob and 2 others like
 For more than 4 names, the number in and 2 others simply increases.
 */
 
-const likes = names => {
-
+let likes = names => {
+  const len = names.length;
+  if (!len) return 'no one likes this';
+  if (len === 1) return `${names[0]} likes this`;
+  if (len === 2) return `${names[0]} and ${names[1]} like this`;
+  if (len === 3) return `${names[0]}, ${names[1]} and ${names[2]} like this`;
+  if (len > 3) return `${names[0]}, ${names[1]} and ${len - 2} others like this`;
 };
 
 const {expect} = require('chai');
