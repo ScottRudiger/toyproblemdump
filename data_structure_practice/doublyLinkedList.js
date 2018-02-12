54716a104c848034c034fa7364575f48ef1201d6
ScottRudiger <scottrudiger@gmail.com>
2018-02-12 14:19:12 -0800

    implement LinkedList class & push method

--- data_structure_practice/doublyLinkedList.js
+++ data_structure_practice/doublyLinkedList.js
@@ -18,7 +18,23 @@ class Node extends singlyNode {
 }
 
 class LinkedList extends singlyLinkedList {
-
+  constructor(head, ...data) {
+    !arguments.length ? super() : super(head, ...data);
+  }
+  push(datum, ...data) { // O(1) to add one item, O(n) to add multiple
+    // save reference to old tail
+    const oldTail = this.tail;
+    // call super's push on first datum
+    super.push(datum);
+    // set new tail's prev to old tail or, if empty list, null
+    this.tail.prev = oldTail ? oldTail : null;
+    // if multiple pieces of data were passed in, call pushBatch
+    if (data.length) this.pushBatch(data);
+  }
+  pushBatch(data) { // O(n)
+    // call push on each piece of data
+    for (const datum of data) this.push(datum);
+  }
 }
 
 module.exports = {Node, LinkedList};
--- data_structure_practice/doublyLinkedList.test.js
+++ data_structure_practice/doublyLinkedList.test.js
@@ -28,3 +28,75 @@ describe('Node class', () => {
     expect(n.prev.data).to.be.null;
   });
 });
+
+describe('LinkedList class', () => {
+  it('should be a class', () => {
+    expect(new LinkedList() instanceof LinkedList);
+  });
+  it('should accept multiple unlinked Nodes', () => {
+    const n1 = new Node(1);
+    const n2 = new Node(2);
+    const n3 = new Node(3);
+    const l = new LinkedList(n1, n2, n3);
+    expect(l.tail).to.equal(n3);
+    expect(l.tail.prev).to.equal(n2);
+    expect(l.tail.prev.prev).to.equal(n1).and.to.equal(l.head);
+    expect(l.tail.next).to.be.null;
+    expect(l.head.prev).to.be.null;
+  });
+  it('should accept multiple pieces of data', () => {
+    const l = new LinkedList(1, 2, 3);
+    expect(l.tail.next).to.be.null;
+    expect(l.tail.data).to.equal(3);
+    expect(l.tail.prev.data).to.equal(2);
+    expect(l.tail.prev.prev.data).to.equal(1);
+    expect(l.head.data).to.equal(1);
+    expect(l.head.prev).to.be.null;
+  });
+  it('should accept previously linked Nodes', () => {
+    const n3 = new Node(3);
+    const n2 = new Node(2, n3);
+    const n1 = new Node(1, n2);
+    const l = new LinkedList(n1, n2, n3);
+    expect(l.tail.next).to.be.null;
+    expect(l.tail).to.equal(n3);
+    expect(l.tail.prev).to.equal(n2);
+    expect(l.tail.prev.prev).to.equal(n1);
+    expect(l.head.prev).to.be.null;
+  });
+  it('should accept a mixture of data and linked/unlinked Nodes', () => {
+    const n = new Node(2);
+    const n2 = new Node(4);
+    const n1 = new Node(3, n2);
+    const l = new LinkedList(1, n, n1, n2);
+    expect(l.tail.next).to.be.null;
+    expect(l.tail).to.equal(n2);
+    expect(l.tail.prev).to.equal(n1);
+    expect(l.tail.prev.prev).to.equal(n);
+    expect(l.head.data).to.equal(1);
+    expect(l.head.prev).to.be.null;
+  });
+
+  context('push method', () => {
+    it('should add a Node to the end of the LinkedList', () => {
+      const l = new LinkedList(0, 1, 2);
+      const n = new Node(3);
+      l.push(n);
+      expect(l.tail).to.equal(n);
+      expect(l.tail.prev.data).to.equal(2);
+    });
+    it('should add a Node if passed data', () => {
+      const l = new LinkedList();
+      l.push(2);
+      expect(l.tail.data).to.equal(2);
+    });
+    it('should add multiple', () => {
+      const l = new LinkedList(0);
+      const n = new Node(2);
+      l.push(1, n);
+      expect(l.tail).to.equal(n);
+      expect(l.tail.prev.data).to.equal(1);
+      expect(l.head.data).to.equal(0);
+    });
+  });
+});
