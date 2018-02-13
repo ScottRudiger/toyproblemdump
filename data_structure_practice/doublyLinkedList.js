f147aca87b00a3c23321a4b38316484a845a8d2f
ScottRudiger <scottrudiger@gmail.com>
2018-02-12 22:24:57 -0800

    (refactor) switch from O(1) size method (tail index prop + 1) to list size prop, incremented/decremented by each operation

--- data_structure_practice/doublyLinkedList.js
+++ data_structure_practice/doublyLinkedList.js
@@ -20,6 +20,9 @@ class Node extends singlyNode {
 class LinkedList extends singlyLinkedList {
   constructor(head, ...data) {
     !arguments.length ? super() : super(head, ...data);
+    // size prop will be created w/ first push operation; however,
+    // to handle case of creating an empty list, set size to 0
+    this.size = ~~this.size;
   }
   push(datum, ...data) { // O(1) to add one item, O(n) to add multiple
     // save reference to old tail
@@ -28,8 +31,9 @@ class LinkedList extends singlyLinkedList {
     super.push(datum);
     // set new tail's prev to old tail or, if empty list, null
     this.tail.prev = oldTail ? oldTail : null;
-    // set new tail's index (setting indices as Nodes are added so size method does not need to iterate)
-    this.tail.index = oldTail ? oldTail.index + 1 : 0;
+    // // set new tail's index (setting indices as Nodes are added so size method does not need to iterate--update: changed to size prop, updated by each operation)
+    // this.tail.index = oldTail ? oldTail.index + 1 : 0;
+    this.size = ~~this.size + 1;
     // if multiple pieces of data were passed in, call pushBatch
     if (data.length) this.pushBatch(data);
   }
@@ -37,17 +41,19 @@ class LinkedList extends singlyLinkedList {
     // call push on each piece of data
     for (const datum of data) this.push(datum);
   }
-  size() { // O(1) by adding index property to each Node during push operation
-    // if list is empty, return 0; otherwise, return the tail's index + 1
-    return this.tail ? this.tail.index + 1 : 0;
-  }
+  // switched to size property, updated by each operation rather than Node index props
+  // size() { // O(1) by adding index property to each Node during push operation
+  //   // if list is empty, return 0; otherwise, return the tail's index + 1
+  //   return this.tail ? this.tail.index + 1 : 0;
+  // }
   traverseRight(fn) { // O(n) linear
     // start traversal at tail
     let node = this.tail;
+    let i = this.size;
     // until node is null,
     while(node) {
       // apply fn to the node
-      fn(node, node.index, this);
+      fn(node, --i, this);
       // set node to node's prev
       node = node.prev;
     }
@@ -59,6 +65,8 @@ class LinkedList extends singlyLinkedList {
     this.tail = removed.prev;
     // update tail's next to null
     this.tail.next = null;
+    // update size
+    this.size--;
     // return the removed Node
     return removed;
   }
--- data_structure_practice/doublyLinkedList.test.js
+++ data_structure_practice/doublyLinkedList.test.js
@@ -100,17 +100,6 @@ describe('LinkedList class', () => {
     });
   });
 
-  context('size method', () => {
-    it('should return 0 for an empty list', () => {
-      const l = new LinkedList();
-      expect(l.size()).to.equal(0);
-    });
-    it('should return 3 for a list with 3 Nodes', () => {
-      const l = new LinkedList(1, 2, 3);
-      expect(l.size()).to.equal(3);
-    });
-  });
-
   context('traverseRight method', () => {
     it('should not throw on an empty list', () => {
       const l = new LinkedList();
@@ -163,6 +152,7 @@ describe('LinkedList class', () => {
     afterEach(() => {
       l.head = null;
       l.tail = null;
+      l.size = 0;
     });
     it('should throw an error if index is negative', () => {
       expect(() => l.insertAt(-2, 2)).to.throw('insertAt: index must be > -1');
@@ -171,33 +161,39 @@ describe('LinkedList class', () => {
       l.insertAt(23, 2);
       expect(l.head.data).to.equal(2);
       expect(l.tail.data).to.equal(2);
-      expect(l.tail.index).to.equal(0);
     });
     it('should insert data at head when index is 0 for a non-empty list', () => {
       l.push(1, 2, 3);
       l.insertAt(0, 0);
       expect(l.head.data).to.equal(0);
-      expect(l.head.index).to.equal(0);
       expect(l).to.eql(new LinkedList(0, 1, 2, 3));
     });
     it('should insert data at the specified index', () => {
       l.push(0, 1, 3);
       l.insertAt(2, 2);
       expect(l.head.next.next.data).to.equal(2);
-      expect(l.head.next.next.index).to.equal(2);
       expect(l.tail.data).to.equal(3);
-      expect(l.tail.index).to.equal(3);
       expect(l).to.eql(new LinkedList(0, 1, 2, 3));
     });
     it('should insert data at tail if index is >= the list\'s size', () => {
       l.push(0, 1, 2);
       l.insertAt(3, 3);
       expect(l.tail.data).to.equal(3);
-      expect(l.tail.index).to.equal(3);
       l.insertAt(23, 4);
       expect(l.tail.data).to.equal(4);
-      expect(l.tail.index).to.equal(4);
-      expect(l).to.eql(new LinkedList(0, 1, 2, 3, 4));
+      l.insertAt(l.size, 5);
+      expect(l).to.eql(new LinkedList(0, 1, 2, 3, 4, 5));
+    });
+  });
+
+  context('size property', () => {
+    it('should be 0 for an empty list', () => {
+      const l = new LinkedList();
+      expect(l.size).to.equal(0);
+    });
+    it('should be 3 for a list with 3 Nodes', () => {
+      const l = new LinkedList(1, 2, 3);
+      expect(l.size).to.equal(3);
     });
   });
 });
