a81b684759dcd6c72d50d6cfd71344c6ecd37052
ScottRudiger <scottrudiger@gmail.com>
2018-02-15 14:13:13 -0800

    implement enqueue method

--- data_structure_practice/queueLinkedList.js
+++ data_structure_practice/queueLinkedList.js
@@ -7,6 +7,29 @@ class Queue {
   constructor(...data) {
     this.storage = new LinkedList(...data);
   }
+  enqueue(data) { // O(1)
+    // if Queue is empty,
+    if (!this.storage.size) {
+      // create a new Node w/ data
+      const node = new Node(data);
+      // set head
+      this.storage.head = node;
+      // set tail
+      this.storage.tail = node;
+    // otherwise, Queue is not empty,
+    } else {
+      // create a new Node w/ data
+      const node = new Node(data);
+      // set tail's next to node
+      this.storage.tail.next = node;
+      // set node's prev to old tail
+      node.prev = this.storage.tail
+      // and set tail to nodoe
+      this.storage.tail = node;
+    }
+    // increment Queue's size
+    this.storage.size++;
+  }
 }
 
 module.exports = Queue;
