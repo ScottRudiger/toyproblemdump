c77e8533f182b2203c557a539029b8f8fc1ada32
ScottRudiger <scottrudiger@gmail.com>
2018-02-16 18:49:50 -0800

    add tests for push method

--- data_structure_practice/stackLinkedList.test.js
+++ data_structure_practice/stackLinkedList.test.js
@@ -14,4 +14,17 @@ describe('Stack class', () => {
   it('should add data to its storage', () => {
     expect(new Stack(1, 2, 3).storage).to.eql(new LinkedList(1, 2, 3));
   });
+
+  context('push method', () => {
+    it('should add data to an empty Stack', () => {
+      const stack = new Stack();
+      stack.push(1);
+      expect(stack).to.eql(new Stack(1));
+    });
+    it('should add data to a non-empty Stack', () => {
+      const stack = new Stack(0, 1);
+      stack.push(2);
+      expect(stack).to.eql(new Stack(0, 1, 2));
+    });
+  });
 });
