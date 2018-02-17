92de1af0d81c4ec46a463dc75963d009f1355bd9
ScottRudiger <scottrudiger@gmail.com>
2018-02-16 19:10:10 -0800

    add tests for pop method

--- data_structure_practice/stackLinkedList.test.js
+++ data_structure_practice/stackLinkedList.test.js
@@ -27,4 +27,25 @@ describe('Stack class', () => {
       expect(stack).to.eql(new Stack(0, 1, 2));
     });
   });
+
+  context('pop method', () => {
+    const stack = new Stack();
+    afterEach(() => stack.storage.clear());
+    it('should return undefined when the Stack is empty', () => {
+      expect(stack.pop()).to.equal(undefined);
+      expect(stack).to.eql(new Stack());
+    });
+    it('should return and remove the only data when storage size is 1', () => {
+      stack.push(0);
+      expect(stack.pop()).to.equal(0);
+      expect(stack).to.eql(new Stack());
+    });
+    it('should return the most recently added data and remove it', () => {
+      stack.push(0);
+      stack.push(1);
+      stack.push(2);
+      expect(stack.pop()).to.equal(2);
+      expect(stack).to.eql(new Stack(0, 1));
+    });
+  });
 });
