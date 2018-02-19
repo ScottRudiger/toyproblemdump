9ecefb0a776341a74ddb69456003d6b2b09cb9c6
ScottRudiger <scottrudiger@gmail.com>
2018-02-18 18:18:47 -0800

    implement enqueue method

--- data_structure_practice/maxQueue.js
+++ data_structure_practice/maxQueue.js
@@ -11,6 +11,20 @@ class MaxQueue {
     // if enqueueing data at construction, iterate through data and enqueue each
     if (data.length) for (const datum of data) this.enqueue(datum);
   }
+  enqueue(data) {
+    // if the MaxQueue is empty,
+    if (!this.dqStack.peek()) {
+      // push data to the dequeue Stack and indicate it's the max;
+      this.dqStack.push([data, data]); // e.g., enqueue(8) ==> [data: 8, max: 8]
+    } else {
+      // otherwise, the MaxQueue is not empty; push data to enqueue Stack
+      this.eqStack.push([data]);
+      // save a reference to the tuple that's next in line to be dequeued
+      const next = this.dqStack.peek();
+      // if the enqueueing data is > the max in that tuple, update it
+      if (data > next[1]) next[1] = data;
+    }
+  }
 }
 
 module.exports = MaxQueue;
