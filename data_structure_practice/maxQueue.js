b92295d9f3f79ae67e124dc28d88c2427de08f4d
ScottRudiger <scottrudiger@gmail.com>
2018-02-18 17:43:10 -0800

    add tests for MaxQueue class

---
+++ data_structure_practice/maxQueue.js
@@ -0,0 +1,22 @@
+// implement a queue with O(1) time complexity for insertion, deletion, and finding the maximum value
+
+class MaxQueue {
+
+}
+
+const {expect} = require('chai');
+
+describe('MaxQueue class', () => {
+  const m = new MaxQueue();
+  it('should store data in a Queue', () => {
+    expect(m.storage).to.be.an.instanceof(Queue);
+  });
+  it('should store maximum values in a Queue', () => {
+    expect(m.max).to.be.an.instanceof(Queue);
+  });
+  it('should use Queues that store data in a linked list', () => {
+    const {LinkedList} = require('./doublyLinkedList');
+    expect(m.storage.storage).to.be.an.instanceof(LinkedList);
+    expect(m.max.storage).to.be.an.instanceof(LinkedList);
+  });
+});
