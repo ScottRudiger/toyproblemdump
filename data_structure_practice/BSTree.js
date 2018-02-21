c08a586be732dab1b0743e6f1ec02fed9ad8eae6
ScottRudiger <scottrudiger@gmail.com>
2018-02-21 11:39:12 -0800

    implement insert method

--- data_structure_practice/BSTree.js
+++ data_structure_practice/BSTree.js
@@ -30,6 +30,46 @@ class BSTree {
     // set comparator; default to a > b
     this.comp = comp;
   }
+  insert(...data) {
+    // iterate through data in case we're inserting multiple
+    for (const datum of data) {
+      // wrap data in Node if it's not already
+      const nodeToInsert = datum instanceof Node
+      ? datum
+      : new Node(datum);
+      // if no root defined yet,
+      if (!this.root) {
+        // set root to value
+        this.root = nodeToInsert;
+        continue;
+      }
+      // otherwise, start at root
+      let node = this.root;
+      while (true) {
+        // if value > node,
+        if (this.comp(nodeToInsert, node)) {
+          // and node's right is null
+          if (!node.right) {
+            // set node's right to value
+            node.right = nodeToInsert;
+            break;
+          }
+          // otherwise, node's right is defined, move right
+          node = node.right;
+        // otherwise, value < node,
+        } else {
+          // if node's left is null,
+          if (!node.left) {
+            // set node's left to value
+            node.left = nodeToInsert;
+            break;
+          }
+          // otherwise, node's left is defined, move left
+          node = node.left;
+        }
+      }
+    }
+  }
 }
 
 module.exports = {Node, BSTree};
