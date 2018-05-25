1b7e209e7ce2eb222c99fa5876fb40dcc1078aec
ScottRudiger <scottrudiger@gmail.com>
2018-05-25 00:20:31 -0800

    Solve w/o using a for loop

--- data_structure_review/exercises/reversestring/index.js
+++ data_structure_review/exercises/reversestring/index.js
@@ -10,12 +10,20 @@
 // const reverse = str => [...str].reverse().join``;
 
 // solve w/o reverse
+// const reverse = str => {
+//   const arr = [...str];
+//   for (let i = 0, j = str.length - 1; i <= j; i++, j--) {
+//     [arr[i], arr[j]] = [arr[j], arr[i]];
+//   }
+//   return arr.join``;
+// };
+
+// solve w/o for loop
 const reverse = str => {
-  const arr = [...str];
-  for (let i = 0, j = str.length - 1; i <= j; i++, j--) {
-    [arr[i], arr[j]] = [arr[j], arr[i]];
-  }
-  return arr.join``;
+  let reversed = '';
+  for (const char of str)
+    reversed = char + reversed;
+  return reversed;
 };
 
 module.exports = reverse;
