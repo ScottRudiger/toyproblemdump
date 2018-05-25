51b62d142e798de317b2b13d839207603982be91
ScottRudiger <scottrudiger@gmail.com>
2018-05-25 00:15:43 -0800

    Solve w/o using `Array.prototype.reverse`

--- data_structure_review/exercises/reversestring/index.js
+++ data_structure_review/exercises/reversestring/index.js
@@ -2,10 +2,20 @@
 // Given a string, return a new string with the reversed
 // order of characters
 // --- Examples
-//   reverse('apple') === 'leppa'
+//   reverse('apple') === 'elppa'
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-const reverse = str => [...str].reverse().join``;
+// solve w/ reverse
+// const reverse = str => [...str].reverse().join``;
+
+// solve w/o reverse
+const reverse = str => {
+  const arr = [...str];
+  for (let i = 0, j = str.length - 1; i <= j; i++, j--) {
+    [arr[i], arr[j]] = [arr[j], arr[i]];
+  }
+  return arr.join``;
+};
 
 module.exports = reverse;
--- data_structure_review/exercises/reversestring/test.js
+++ data_structure_review/exercises/reversestring/test.js
@@ -11,3 +11,11 @@ test('Reverse reverses a string', () => {
 test('Reverse reverses a string', () => {
   expect(reverse('  abcd')).toEqual('dcba  ');
 });
+
+test('Reverse reverses an odd length string', () => {
+  expect(reverse('  abcd ')).toEqual(' dcba  ');
+});
+
+test('Reverse does not fail on an empty string', () => {
+  expect(reverse('')).toEqual('');
+});
