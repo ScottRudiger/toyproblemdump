355113955ff3723b1d0a8075b848ba0f2561eef9
ScottRudiger <scottrudiger@gmail.com>
2018-05-26 00:54:48 -0800

    Alternate solution

--- data_structure_review/exercises/capitalize/index.js
+++ data_structure_review/exercises/capitalize/index.js
@@ -10,13 +10,23 @@
 // const capitalize = str =>
 //   str.split` `.map(word => word[0].toUpperCase() + word.slice(1)).join` `;
 
+// const capitalize = str => {
+//   let result = '';
+//   [...str].forEach((char, i) => {
+//     if (!i || str[i - 1] === ' ')
+//       result += char.toUpperCase();
+//     else
+//       result += char;
+//   });
+//   return result;
+// };
+
 const capitalize = str => {
-  let result = '';
-  [...str].forEach((char, i) => {
-    if (!i || str[i - 1] === ' ')
-      result += char.toUpperCase();
-    else
-      result += char;
+  let result = str[0].toUpperCase();
+  [...str.slice(1)].forEach((char, i) => {
+    result += str[i] === ' '
+      ? char.toUpperCase()
+      : char;
   });
   return result;
 };
