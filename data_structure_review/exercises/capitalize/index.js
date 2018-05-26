e9c48e25014e8c40c447575ad0a0e9e88fa882c1
ScottRudiger <scottrudiger@gmail.com>
2018-05-26 00:51:33 -0800

    Alternate solution

--- data_structure_review/exercises/capitalize/index.js
+++ data_structure_review/exercises/capitalize/index.js
@@ -7,7 +7,18 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-const capitalize = str =>
-  str.split` `.map(word => word[0].toUpperCase() + word.slice(1)).join` `;
+// const capitalize = str =>
+//   str.split` `.map(word => word[0].toUpperCase() + word.slice(1)).join` `;
+
+const capitalize = str => {
+  let result = '';
+  [...str].forEach((char, i) => {
+    if (!i || str[i - 1] === ' ')
+      result += char.toUpperCase();
+    else
+      result += char;
+  });
+  return result;
+};
 
 module.exports = capitalize;
