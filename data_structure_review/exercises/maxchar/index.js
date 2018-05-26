d7998627dfb1470be5393135d21a724fff29a4fc
ScottRudiger <scottrudiger@gmail.com>
2018-05-26 00:06:07 -0800

    Alternative solution

--- data_structure_review/exercises/maxchar/index.js
+++ data_structure_review/exercises/maxchar/index.js
@@ -5,13 +5,29 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
+// const maxChar = str => {
+//   const counts = [...str].reduce((counts, char) => {
+//     counts[char] = (counts[char] || 0) + 1;
+//     return counts;
+//   }, {});
+//   const [[max]] = Object.entries(counts).sort(([, countA], [, countB]) => countB - countA);
+//   return max;
+// };
+
 const maxChar = str => {
+  let maxCount = 0;
+  let maxChar = '';
   const counts = [...str].reduce((counts, char) => {
-    counts[char] = (counts[char] || 0) + 1;
+    counts[char] = counts[char] + 1 || 1;
     return counts;
   }, {});
-  const [[max]] = Object.entries(counts).sort(([, countA], [, countB]) => countB - countA);
-  return max;
-}
+  for (const char in counts) {
+    if (counts[char] > maxCount) {
+      maxCount = counts[char];
+      maxChar = char;
+    }
+  }
+  return maxChar;
+};
 
 module.exports = maxChar;
