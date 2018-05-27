036610c2cc178517a3bcdcaffd93458bb396661c
ScottRudiger <scottrudiger@gmail.com>
2018-05-27 01:42:05 -0800

    Solve iteratively

--- data_structure_review/exercises/vowels/index.js
+++ data_structure_review/exercises/vowels/index.js
@@ -8,6 +8,14 @@
 //   vowels('Why?') --> 0
 
 // solve w/ regex
-const vowels = str => (str.match(/[aeiou]/gi) || []).length;
+// const vowels = str => (str.match(/[aeiou]/gi) || []).length;
+
+// solve iteratively
+const vowels = str => {
+  let count = 0;
+  for (const char of str.toLowerCase())
+    if ([...'aeiou'].includes(char)) count++;
+  return count;
+};
 
 module.exports = vowels;
