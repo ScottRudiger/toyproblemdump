5c1425b54973178b94c5eee7e2a0957d32bf4754
ScottRudiger <scottrudiger@gmail.com>
2018-05-28 00:36:33 -0800

    Iterative solution

--- data_structure_review/exercises/fib/index.js
+++ data_structure_review/exercises/fib/index.js
@@ -9,9 +9,22 @@
 //   fib(4) === 3
 
 // recursive solution
+// const fib = n => {
+//   if (n < 2) return n;
+//   return fib(n - 1) + fib(n - 2);
+// };
+
+// iterative solution
 const fib = n => {
   if (n < 2) return n;
-  return fib(n - 1) + fib(n - 2);
+  let seq = [0, 1];
+  let i = 2;
+  while (i++ <= n) {
+    const prev = seq[1];
+    const curr = seq.reduce((a, b) => a + b);
+    seq = [prev, curr];
+  }
+  return seq[1];
 };
 
 module.exports = fib;
