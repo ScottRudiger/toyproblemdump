1f8a3e805b252b694e4a68e9c5b414f0d80c2955
ScottRudiger <scottrudiger@gmail.com>
2018-06-04 00:05:03 -0800

    Implement bubbleSort

--- data_structure_review/exercises/sorting/index.js
+++ data_structure_review/exercises/sorting/index.js
@@ -1,8 +1,13 @@
 // --- Directions
 // Implement bubbleSort, selectionSort, and mergeSort
 
-function bubbleSort(arr) {
-
+function bubbleSort(arr) { // O(n^2) quadratic time
+  const copy = [...arr];
+  for (let i = 0; i < copy.length; i++)
+    for (let j = 0; j < copy.length - i; j++)
+      if (copy[j] > copy[j + 1])
+        [copy[j], copy[j + 1]] = [copy[j + 1], copy[j]];
+  return copy;
 }
 
 function selectionSort(arr) {
