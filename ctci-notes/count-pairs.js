aac4bd929ace4ff558ae4de502ec39e194ef067f
ScottRudiger <scottrudiger@gmail.com>
2018-06-06 01:23:52 -0800

    Initial solution

--- ctci-notes/count-pairs.js
+++ ctci-notes/count-pairs.js
@@ -1,11 +1,18 @@
 // Given an array of distinct integer values, count the number of pairs of integers that have difference k. For example, given the array [1, 7, 5, 9, 2, 12, 3] and the difference k = 2, there are four pairs with difference 2: [1, 3], [3, 5], [5, 7], [7, 9].
 
-const countPairs = nums => {};
+// brute force O(n log n) because of sort
+const countPairs = (nums, k) => {
+  let count = 0;
+  nums.sort((a, b) => a - b).forEach((num, i) => {
+    nums.slice(i + 1).find(n => n === num + k) && count++;
+  });
+  return count;
+};
 
 const {expect} = require('chai');
 
 describe('countPairs', () => {
   it('works with the given example', () => {
-    expect(countPairs([1, 7, 5, 9, 2, 12, 3])).to.equal(4);
+    expect(countPairs([1, 7, 5, 9, 2, 12, 3], 2)).to.equal(4);
   });
 });
