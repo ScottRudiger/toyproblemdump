3873d2d3460eb6af82a5d6ef5e2df83b75270f5a
ScottRudiger <scottrudiger@gmail.com>
2018-06-06 03:09:11 -0800

    Solve w/o additional data structure

--- ctci-notes/isUnique.js
+++ ctci-notes/isUnique.js
@@ -1,11 +1,19 @@
 // Implement an algorithm to determine if a string has all unique characters. What if you cannot use additional data structures?
 
 // with counts object, O(n) time
-const countChars = str => [...str].reduce((counts, char) => ({ // O(n)
-  ...counts, [char]: (counts[char] || 0) + 1
-}), {});
+// const countChars = str => [...str].reduce((counts, char) => ({ // O(n)
+//   ...counts, [char]: (counts[char] || 0) + 1
+// }), {});
 
-const isUnique = str => Object.values(countChars(str)).every(count => count === 1); // O(n)
+// const isUnique = str => Object.values(countChars(str)).every(count => count === 1); // O(n)
+
+// w/o additional data structures
+const isUnique = str => {
+  for (let i = 0; i < str.length; i++)
+    if (str.slice(i + 1).includes(str[i]))
+      return false;
+  return true;
+};
 
 const {expect} = require('chai');
 
