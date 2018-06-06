4209d46178d0824e5057a3783d0af90204b917bc
ScottRudiger <scottrudiger@gmail.com>
2018-06-06 02:16:53 -0800

    Initial solution

--- ctci-notes/countCommons.js
+++ ctci-notes/countCommons.js
@@ -4,8 +4,13 @@
 // [13, 27, 35, 40, 49, 55, 59]
 // [17, 35, 39, 40, 55, 58, 60]
 
+// O(n^2)
 const countCommons = (arr1, arr2) => {
-
+  let count = 0;
+  arr1.forEach(n => {
+    arr2.includes(n) && count++;
+  });
+  return count;
 };
 
 const {expect} = require('chai');
