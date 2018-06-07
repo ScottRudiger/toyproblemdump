3ffe2a46c04b826224825d5d612876722ae59071
ScottRudiger <scottrudiger@gmail.com>
2018-06-07 00:15:02 -0800

    Alternative solution

--- ctci-notes/checkPermutation.js
+++ ctci-notes/checkPermutation.js
@@ -1,14 +1,21 @@
 // Given two strings, write a function to determine if one is a permutation of the other.
 
-const checkPermutation = (str1, str2) => { // O(n^2)
+// O(n^2)
+// const checkPermutation = (str1, str2) => {
+//   if (str1.length !== str2.length) return false;
+//   str2 = [...str2];
+//   for (const char of str1) {
+//     const index = str2.indexOf(char);
+//     if (index === -1) return false;
+//     str2.splice(index, 1);
+//   }
+//   return str2.length === 0;
+// };
+
+// O(n log n)
+const checkPermutation = (str1, str2) => {
   if (str1.length !== str2.length) return false;
-  str2 = [...str2];
-  for (const char of str1) {
-    const index = str2.indexOf(char);
-    if (index === -1) return false;
-    str2.splice(index, 1);
-  }
-  return str2.length === 0;
+  return [...str1].sort().join`` === [...str2].sort().join``;
 };
 
 const {expect} = require('chai');
